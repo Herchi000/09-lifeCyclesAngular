41511bfb434dda63e12ad2f1a002df5c0d6a746b
unknown <hugofer000@gmail.com>
2022-07-19 17:04:21 -0300

    09-lifeCycles 02

--- src/app/pages/pagina1/pagina1.component.ts
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pagina1',
@@ -6,11 +7,62 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class Pagina1Component implements OnInit {
+export class Pagina1Component implements
+  OnInit, OnChanges, DoCheck, AfterContentChecked, AfterContentInit,
+  AfterViewInit, AfterViewChecked, OnDestroy {
 
-  constructor() { }
+    nombre: string = 'Emanuel';
+    segundos: number = 0;
+    timerSubscription!: Subscription
+
+  constructor() { 
+    console.log('constructor');
+    
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    throw new Error('Method not implemented.');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    this.timerSubscription.unsubscribe();
+    console.log('Timer limpiado');
+  }
 
   ngOnInit(): void {
+   console.log('NgOnInit');
+   this.timerSubscription = interval(1000).subscribe(i => {
+    this.segundos = i;
+    
+   })
+    
+  }
+
+  
+
+
+  guardar(){
+
   }
 
 }
